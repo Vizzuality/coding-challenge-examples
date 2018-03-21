b31dbe70545d2372dbc9f1cd10def38534d8d68b
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-03-21 11:56:45 +0000

    updates babel and other dependencies

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 import App from './components/app';
 import './index.css';
 
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root')
-  );
-};
+const HotRootComponent = hot(module)(App);
 
-render(App);
-
-if (module.hot) {
-  module.hot.accept('./components/app', () => {
-    render(App);
-  });
-}
+ReactDOM.render(<HotRootComponent />, document.getElementById('root'));
